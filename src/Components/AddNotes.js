673983500fe2d32e2abe739bef9640f332bd5838
Nikesh Gamal <nikesh.191624@ncit.edu.np>
2022-05-16 14:46:57 +0545

    add adding note functionality

--- src/Components/AddNotes.js
+++ src/Components/AddNotes.js
@@ -6,12 +6,13 @@ export const AddNotes = () => {
     const context= useContext(noteContext);
     const {addNote} = context;
 
-    const [note, setNote] = useState({title:"",description:"",tags:"Default"});
+    const [note, setNote] = useState({title:"",description:"",tags:""});
     const onSubmit = (e)=>{
         //e.preventDefault() in order to prevent from reloading the pages
         e.preventDefault();
         //addNote method receives 3 parameters that will add the respective note 
          addNote(note.title,note.description,note.tags);
+         setNote({title:"",description:"",tags:""});
     }
 
     const onChange = (e)=>{
@@ -25,18 +26,18 @@ export const AddNotes = () => {
          <form>
              <div className="form-group">
                  <label htmlFor="title">Your Title</label>
-                 <input type="text" className="form-control my-2" id="title" name="title" aria-describedby="emailHelp" placeholder="Enter title" onChange={onChange}/>
+                 <input type="text" className="form-control my-2" id="title" name="title" aria-describedby="emailHelp" placeholder="Enter title" minLength={5} onChange={onChange} value={note.title}/>
              </div>
              <div className="form-group">
                   <label htmlFor="description">Description</label>
-                  <input type="text" className="form-control my-2" id="description" name="description" placeholder="Description" onChange={onChange}/>
+                  <input type="text" className="form-control my-2" id="description" name="description" placeholder="Description" minLength={5} onChange={onChange} value={note.description}/>
              </div>
              <div className="form-group">
                   <label htmlFor="description">Tags</label>
-                  <input type="text" className="form-control my-2" id="tags" name="tags" placeholder="Tags" onChange={onChange}/>
+                  <input type="text" className="form-control my-2" id="tags" name="tags" placeholder="Tags" onChange={onChange} value={note.tags}/>
              </div>
 
-            <button type="submit" className="btn btn-primary  my-2" onClick={onSubmit}>Add a note</button>
+            <button disabled={note.title.length<5 || note.description.length<5 ?true:false} type="submit" className="btn btn-primary  my-2" onClick={onSubmit}>Add a note</button>
          </form>
     </div>
   )
