a3a05e42195f2b2c4815f2cebcc98973bef4933f
Nikesh Gamal <nikesh.191624@ncit.edu.np>
2022-05-16 14:40:00 +0545

    added sign-up functionality

---
+++ src/Components/Signup.js
@@ -0,0 +1,62 @@
+import React,{useState} from 'react'
+import { useNavigate} from 'react-router-dom'
+
+const Signup = () => {
+  //Use of the useState hook should be inside a component if it is placed inside handleSubmit then it will show an error as handleSubmit is not a component of the React or function used in useState hook
+  const [credentials, setCredentials] = useState({name:"",email:"",password:"",cpassword:""})
+  let navigate = useNavigate();
+
+  const handleSubmit = async(e)=>{
+      e.preventDefault();
+      const {name,email,password}=credentials;
+      //hitting the api-->endpoints
+      const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json'
+         },
+        body: JSON.stringify({name,email,password}) 
+      });
+      const json= await response.json();
+      console.log(json);
+
+      if(json.success){
+          //save the token and refirect
+          localStorage.setItem('token',json.authtoken);
+           //for redirecting we use --------useHistory hook in reac-router-5 but after react-router 6 we use navigate hook to redirect--------- hook 
+          navigate("/");
+      }
+  }
+
+  const onChange = (e)=>{
+    //With every change on the values of the element we see the changes over the value
+    setCredentials({...credentials,[e.target.name]:e.target.value});
+}
+  return (
+    <div className='container'>
+       <form onSubmit={handleSubmit}>
+       <div className="mb-3">
+             <label htmlFor="name" className="form-label">Enter name</label>
+             <input type="text" className="form-control" id="name" aria-describedby="emailHelp" name='name' value={credentials.name} onChange={onChange}/>
+             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+           </div>
+           <div className="mb-3">
+             <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+             <input type="email" className="form-control" id="email" aria-describedby="emailHelp" name='email' value={credentials.email} onChange={onChange}/>
+             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+           </div>
+           <div className="mb-3">
+             <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+             <input type="password" className="form-control" id="password" name='password' value={credentials.password} onChange={onChange}/>
+           </div>
+           <div className="mb-3">
+             <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+             <input type="password" className="form-control" id="cpassword" name='cpassword' value={credentials.cpassword} onChange={onChange}/>
+           </div>
+           <button type="submit" className="btn btn-primary">Submit</button>
+       </form>
+    </div>
+  )
+}
+
+export default Signup
